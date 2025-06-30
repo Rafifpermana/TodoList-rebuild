c0a73287dd1f776918dea1ef262653c9ac48a076
DRIV32 <rafifpermana39@gmail.com>
2025-06-30 21:12:50 +0700

    input api key

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Konfigurasi Firebase Anda (dari Firebase Console)
 const firebaseConfig = {
-  apiKey: "AIzaSyBbyugQBkcjpP9dI9tEYFXyNxufEF8hMns",
-  authDomain: "mysmartnotebook-df42b.firebaseapp.com",
-  projectId: "mysmartnotebook-df42b",
-  storageBucket: "mysmartnotebook-df42b.firebasestorage.app",
-  messagingSenderId: "1084724733648",
-  appId: "1:1084724733648:web:59bf3a7261cbef438860d5",
-  measurementId: "G-W13H242SGM",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
 // Inisialisasi Firebase
